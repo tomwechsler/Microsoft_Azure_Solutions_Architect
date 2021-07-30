ede7b66cdaaea1226a49f0ddc6ed16d7daef56c5
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-07-30 07:14:54 +0200

    Add files via upload

---
+++ 02_02_SimplePaas.ps1
@@ -0,0 +1,56 @@
+﻿Set-Location c:\
+Clear-Host
+
+#Install Az Module
+Install-Module -Name Az -AllowClobber -Force -Verbose
+
+#Import Az Module and Authenticate to Azure
+Import-Module -Name Az
+
+#Log into Azure
+Connect-AzAccount
+
+#Select the correct subscription
+Get-AzSubscription -SubscriptionName "MSDN Platforms" | Select-AzSubscription
+Get-AzContext
+
+#Define Deployment Variables
+$appNamePrefix = 'tws'
+$resourceGroupName = "$appNamePrefix-simple-paas"
+$resourceGroupLocation = 'westeurope'
+
+$resourceProviderNamespace = 'Microsoft.Web'
+$resourceTypeName = 'sites'
+
+$randomString = ([char[]]([char]'a'..[char]'z') + 0..9 | Sort-Object {Get-Random})[0..8] -join ''
+$appServicePlanName = $appNamePrefix + $randomString
+$webAppName = $appNamePrefix + $randomString
+
+#Get ARM Provider Locations
+((Get-AzResourceProvider `
+    -ProviderNamespace "$resourceProviderNamespace").ResourceTypes | `
+    Where-Object {$_.ResourceTypeName -eq "$resourceTypeName"}).Locations | `
+    Sort-Object
+
+#Create ARM Resource Group
+New-AzResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Verbose -Force
+
+#Create App Service Plan
+$appServicePlan = New-AzAppServicePlan `
+    -ResourceGroupName $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Name $appServicePlanName `
+    -Tier Standard `
+    -WorkerSize Small `
+    -Verbose
+
+#Create Web App
+New-AzWebApp `
+    -ResourceGroupName $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -AppServicePlan $appServicePlan.ServerFarmWithRichSkuName `
+    -Name $webAppName `
+    -Verbose
